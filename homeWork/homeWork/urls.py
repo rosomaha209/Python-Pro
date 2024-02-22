51b66ae91cef7bb9ae8bf5eb8c7a4501efc62a44
Роман Слюсар <rosomaha209@gmail.com>
2024-02-22 23:27:23 +0200

    refactor: rename project

--- homeWork/homeWork/urls.py
+++ homeWork/homeWork/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for homeWork_5 project.
+URL configuration for homeWork project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- homeWork/homeWork/wsgi.py
+++ homeWork/homeWork/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for homeWork_5 project.
+WSGI config for homeWork project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homeWork_5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homeWork.settings')
 
 application = get_wsgi_application()
