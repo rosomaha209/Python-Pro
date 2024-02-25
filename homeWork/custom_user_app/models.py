6429abfd912b19da02568c779fa68dd44c29182e
Roman <rosomaha209@gmail.com>
2024-02-25 12:22:34 +0100

    feat: add custom user

--- homeWork/custom_user_app/models.py
+++ homeWork/custom_user_app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True)
-    email = models.EmailField(unique=True,help_text='Required. Enter a valid email address.')
+    email = models.EmailField(unique=True, help_text='Required. Enter a valid email address.')
